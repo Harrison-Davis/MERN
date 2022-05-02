9bb1300d11f400609e0eafff0240880fd9981bb9
Harrison-Davis <99163784+Harrison-Davis@users.noreply.github.com>
2022-05-02 12:12:20 -0700

    original

---
+++ assignments/core_assignments/predict_destructuring.js
@@ -0,0 +1,76 @@
+// Problem 1
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+// logs tesla
+// logs mercedes
+
+
+// Problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+// Error, name not defined outside of employee
+
+
+
+// Problem 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+// Person does not have password key, will give an error
+// Prints passwrd, but hash is undefined
+
+// Problem 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// sets first as 2
+// sets second as 5
+// sets third as 2
+// prints false
+// prints true
+
+
+
+// Problem 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+// set key as value
+// sets secondkey as given array
+// sets willthiswork as second value in array, 5
+// logs vlaue
+// logs given rray
+// logs  1
+// logs  5
