1ffbbcff9b9f1946616ec3816bd1fbeed6618494
Harrison-Davis <99163784+Harrison-Davis@users.noreply.github.com>
2022-05-04 17:03:39 -0700

    putting it together(counter)

--- intro_to_react/first-jsx/src/App.js
+++ intro_to_react/first-jsx/src/App.js
@@ -4,15 +4,14 @@ import PersonCard from './components/PersonCard';
 
 
 function App() {
+
   return (
     <div className="App">
       <h1>Hello</h1>
-      <PersonCard firstName="Jane" lastName="Doe" age="45" hairColor="Black"/>
-      <PersonCard firstName="John" lastName="Smith" age="88" hairColor="Brown"/>
-      <PersonCard firstName="Fillmore" lastName="Millard" age="50" hairColor="Brown"/>
-      <PersonCard firstName="Maria" lastName="Smith" age="62" hairColor="Brown"/>
-
-
+      <PersonCard firstName="Jane" lastName="Doe" age={44} hairColor="Black"/>
+      <PersonCard firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
+      <PersonCard firstName="Fillmore" lastName="Millard" age={50} hairColor="Brown"/>
+      <PersonCard firstName="Maria" lastName="Smith" age={62} hairColor="Brown"/>
     </div>
   );
 }
--- intro_to_react/first-jsx/src/components/PersonCard.js
+++ intro_to_react/first-jsx/src/components/PersonCard.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react';
 
+
+
 class PersonCard extends Component{
+
+    // Delcare construcor to override our default 
+    // constructor so that we can initially set the state object.
+    constructor(props){
+        // gives us all the functionality of the edfault constructor that comes with component
+        super(props);
+        // this. refers to scope. state is an object that holds attributes/
+        this.state = {
+            firstName: this.props.firstName,
+            lastName: this.props.lastName,
+            age: this.props.age,
+            hairColor: this.props.hair
+        }
+    }
+
+    // modify age of state
+    increaseAge = () => {
+        console.log("clicked");
+        this.setState({
+            age: this.state.age +=1
+        }, () => console.log(this.state.num))
+    }
+
+    // modify age of object
+    increaseAge1 = () => {
+        console.log("clicked");
+        this.setState({
+            age: this.state.age + 1
+        }, () => console.log(this.state.num))
+        // if you want to console log within state
+    }
+
+        increaseAge2 = () => {
+            console.log("clicked");
+            this.setState({
+                PersonCard: this.state.age += 1
+            })
+        }
+
+
     render(){
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName} </h1>
-                <p> Age: {this.props.age}</p>
+                <p> Age: {this.state.age}</p>
                 <p> Hair Color: {this.props.hairColor}</p>
+                <button onClick={() => this.increaseAge1()}>Increase Age</button>
             </div>
         )
     }
